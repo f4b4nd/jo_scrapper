848b68ad383092f1e8818e68192862eb7fcf3ace
fabien <fabien.andrian@gmail.com>
2019-11-19 06:05:55 +0100

    using argparse

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from journalofficiel.scraper import JOScraper
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("start", help="starting date in DD/MM/YYYY", type=str)
+parser.add_argument("end", help="ending date in DD/MM/YYYY", type=str)
+parser.add_argument("--doc", default='Decrets',
+                    choices=['Decrets', 'Demandes'], type=str)
+args = parser.parse_args()
+
 
 if __name__ == '__main__':
 
-    start = input('Enter starting date in DD/MM/YYYY: ')
-    end = input('Enter ending date in DD/MM/YYYY: ')
-
-    jo_scraper = JOScraper(start, end)
+    jo_scraper = JOScraper(args.start, args.end)
     for date in jo_scraper.period():
-        jo_scraper.run(date, 'Decrets')
+        jo_scraper.run(date, args.doc)

47ff7b79e75d14ef05bd1e25e8e88dfc1ec36146
f4b4nd <fabien.andrian@gmail.com>
2019-12-05 15:52:33 +0100

    add default today as end date

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from journalofficiel.scraper import JOScraper
 import argparse
+import datetime as dt
 
-
+def today():
+  today = dt.date.today()
+  return today.strftime("%d/%m/%Y")
+  
+  
 parser = argparse.ArgumentParser()
 parser.add_argument("start", help="starting date in DD/MM/YYYY", type=str)
-parser.add_argument("end", help="ending date in DD/MM/YYYY", type=str)
+parser.add_argument("--end", default=today(),
+                    help="ending date in DD/MM/YYYY", type=str)
 parser.add_argument("--doc", default='Decrets',
                     choices=['Decrets', 'Demandes'], type=str)
 args = parser.parse_args()
